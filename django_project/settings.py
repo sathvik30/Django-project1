2dcbb80913c8bd316c7f17cca62271784da61256
sathvik200530 <23eg105c39@anurag.edu.in>
2026-06-19 12:38:34 +0530

    Add email timeout

--- django_project/settings.py
+++ django_project/settings.py
@@ -141,9 +141,10 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+EMAIL_TIMEOUT = 30
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True

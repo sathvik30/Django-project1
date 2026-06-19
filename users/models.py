7fc7aecadc675b3cd6548844ce183cea71fcac69
sathvik200530 <23eg105c39@anurag.edu.in>
2026-06-19 16:43:41 +0530

    Move default image into profile_pics

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 # Create your models here.
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
-  image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
+  image = models.ImageField(default='profile_pics/default.jpeg', upload_to='profile_pics')
 
   def __str__(self):
     return f'{self.user.username} profile'
